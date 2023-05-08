4c91516581a8c7ed0652867189a7191d83bce1b4
LNUAKX <lukmaannadaf@gmail.com>
2023-05-08 14:40:24 +0530

    l

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer>
-      <p>Copyright ⓒukn {year}</p>
+      <p>Copyright ⓒlukn {year}</p>
     </footer>
   );
 }
